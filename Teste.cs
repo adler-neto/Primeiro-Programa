5e9bde8281e934f8a958b8e00497cd060b55da18
Adler Neto <adler.neto@outlook.com.br>
2022-06-18 15:10:25 -0300

    teste

---
+++ Teste.cs
@@ -0,0 +1,25 @@
+namespace Teste
+{
+    public class Contas
+    {
+        public int Soma(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtracao(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Divisao(int a, int b)
+        {
+            return a / b;
+        }
+
+        public int Multplicacao(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
